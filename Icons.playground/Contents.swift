f1ecb1d9a1c1f36f41aa109715504b48feb0a1e6
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2016-03-31 15:41:45 +0200

    Added comment about how to view the icons

--- Icons.playground/Contents.swift
+++ Icons.playground/Contents.swift
@@ -1,11 +1,13 @@
 
+// Animated iOS icons created by Matěj Jirásek
+// Open assistant editor to view icons, if you click them, they will animate
+
 import UIKit
 import XCPlayground
 
 let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 200))
 
-let rect = CGRect(x: 0, y: 0, width: 100, height: 100)
-let checkmark = MKJCheckmark(frame: rect)
+let checkmark = MKJCheckmark(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 let burger = MKJBurgerIcon(frame: CGRect(x: 0, y: 100, width: 100, height: 100))
 
 view.addSubview(checkmark)
