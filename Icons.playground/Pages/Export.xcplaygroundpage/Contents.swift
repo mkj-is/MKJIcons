ca7759ddcdeacfae784b6729ecfe4b8d25fc2479
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2016-04-16 12:38:45 +0200

    Exporter animation time settings

--- Icons.playground/Pages/Export.xcplaygroundpage/Contents.swift
+++ Icons.playground/Pages/Export.xcplaygroundpage/Contents.swift
@@ -1,11 +1,13 @@
+//: [Previous](@previous)
 
-// For exporting files you need to have "Shared Playground Data" folder in your user Documents folder
+//: For exporting files you need to have "Shared Playground Data" folder in your user Documents folder
 
 import UIKit
 import XCPlayground
 
 let icon = InteractiveCheckmarkIcon(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 
-let exporter = AnimatedIconExporter(icon: icon, folder: XCPlaygroundSharedDataDirectoryURL)
+let folder = XCPlaygroundSharedDataDirectoryURL.URLByAppendingPathComponent("checkmark_forward")
+let exporter = AnimatedIconExporter(icon: icon, folder: folder, direction: .ForwardAndBack)
 
 exporter.save()
--- Icons.playground/Sources/Helpers/AnimatedIconExporter.swift
+++ Icons.playground/Sources/Helpers/AnimatedIconExporter.swift
@@ -1,14 +1,22 @@
 import UIKit
 
+public enum AnimatedIconExporterDirection {
+    case Forward
+    case Backward
+    case ForwardAndBack
+}
+
 public class AnimatedIconExporter {
     
     var icon: AnimatedIcon
+    var direction: AnimatedIconExporterDirection
     var count: Int
     var frames: [UIImage]
     var folder: NSURL
     
-    public init(icon: AnimatedIcon, folder: NSURL, count: Int = 50) {
+    public init(icon: AnimatedIcon, folder: NSURL, direction: AnimatedIconExporterDirection = .Forward, count: Int = 50) {
         self.icon = icon
+        self.direction = direction
         self.count = count
         self.folder = folder
         self.frames = []
@@ -18,11 +26,24 @@ public class AnimatedIconExporter {
         frames = []
         for i in 0...count {
             UIGraphicsBeginImageContext(icon.frame.size)
-            icon.draw(CGFloat(i) / CGFloat(count))
+            icon.draw(position(i))
             frames.append(UIGraphicsGetImageFromCurrentImageContext())
             UIGraphicsEndImageContext()
         }
         return frames
+    }
+    
+    func position(i: Int) -> CGFloat {
+        let value = (CGFloat(i) / CGFloat(count)) % 1
+        
+        switch direction {
+        case .Forward:
+            return value
+        case .Backward:
+            return 1 - value
+        case .ForwardAndBack:
+            return (value % 0.5) * 2 * (value >= 0.5 ? -1 : 1) + (value >= 0.5 ? 1 : 0)
+        }
     }
     
     public func save() -> Bool {
@@ -30,17 +51,16 @@ public class AnimatedIconExporter {
         var exported = true
         
         let name = NSStringFromClass(icon.dynamicType).componentsSeparatedByString(".").last
-        let assetFolder = folder.URLByAppendingPathComponent(name!)
         
-        if !NSFileManager.defaultManager().fileExistsAtPath(assetFolder.path!) {
-            try! NSFileManager.defaultManager().createDirectoryAtURL(assetFolder, withIntermediateDirectories: true, attributes: nil)
+        if !NSFileManager.defaultManager().fileExistsAtPath(folder.path!) {
+            try! NSFileManager.defaultManager().createDirectoryAtURL(folder, withIntermediateDirectories: true, attributes: nil)
         }
         
         let images = export()
         for i in 0...(images.count - 1) {
             
             let fileName = String(format: "%@_%03d.png", arguments: [name!, i])
-            let filePath = assetFolder.URLByAppendingPathComponent(fileName)
+            let filePath = folder.URLByAppendingPathComponent(fileName)
             let data = UIImagePNGRepresentation(images[i])
             
             if let data = data {
