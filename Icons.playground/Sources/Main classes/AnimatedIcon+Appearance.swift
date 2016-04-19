63bf6e7f06097feb739abe044a00d174788bd210
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2016-04-19 17:58:08 +0200

    Icons support appearance protocol

---
+++ Icons.playground/Sources/Main classes/AnimatedIcon+Appearance.swift
@@ -0,0 +1,68 @@
+//
+//  AnimatedIcon+Appearance.swift
+//  MKJIcons
+//
+//  Created by Matěj Jirásek on 19/04/16.
+//  Copyright © 2016 Matěj Kašpar Jirásek. All rights reserved.
+//
+
+import UIKit
+
+public extension AnimatedIcon {
+    
+    public func appearanceLineWidth() -> CGFloat {
+        return lineWidth
+    }
+    
+    public func setAppearanceLineWidth(property: CGFloat) {
+        lineWidth = property
+    }
+    
+    
+    public func appearanceLineCapStyle() -> Int {
+        return lineCapStyle
+    }
+    
+    public func setAppearanceLineCapStyle(property: Int) {
+        lineCapStyle = property
+    }
+    
+    
+    public func appearanceLineJoinStyle() -> Int {
+        return lineJoinStyle
+    }
+    
+    public func setAppearanceLineJoinStyle(property: Int) {
+        lineJoinStyle = property
+    }
+    
+    
+    public func appearanceAnimationDuration() -> CGFloat {
+        return CGFloat(animationDuration)
+    }
+    
+    public func setAppearanceAnimationDuration(property: CGFloat) {
+        animationDuration = Double(property)
+    }
+    
+    
+    public func appearanceAnimationTimingFunction() -> Int {
+        return animationTimingFunction
+    }
+    
+    public func setAppearanceAnimationTimingFunction(property: Int) {
+        animationTimingFunction = property
+    }
+    
+    
+    public func appearanceAnimationColorMode() -> Int {
+        return animationColorMode
+    }
+    
+    public func setAppearanceAnimationColorMode(property: Int) {
+        animationColorMode = property
+    }
+    
+    
+    
+}
