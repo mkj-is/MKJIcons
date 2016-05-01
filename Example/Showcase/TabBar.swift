0d83ca395df1616d2b94b29e1e07b6b6378f1e4d
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2016-05-01 12:17:35 +0200

    Tab icons in example are redrawn too

--- Example/Showcase/TabBar.swift
+++ Example/Showcase/TabBar.swift
@@ -16,15 +16,39 @@ class TabBar: UITabBar {
         
         tintColor = UIColor.iconOrangeColor
         
+        styleChanged(nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(styleChanged(_:)), name: Notification.StyleChange.rawValue, object: nil)
+    }
+    
+    func styleChanged(notification: NSNotification?) {
+        
+        var style = Style()
+        
+        if let notification = notification, let userInfo = notification.userInfo, let s = userInfo["style"] as? Style {
+            style = s
+        }
+        
         let iconFrame = CGRect(x: 0, y: 0, width: 60, height: 60)
         
-        let starIcon = StarIcon(frame: iconFrame).image(1)
-        let settingsIcon = SettingsIcon(frame: iconFrame).image()
+        let starIcon = StarIcon(frame: iconFrame)
+        let settingsIcon = SettingsIcon(frame: iconFrame)
+        
+        starIcon.applyStyle(style)
+        settingsIcon.applyStyle(style)
         
         if let items = self.items {
-            items[0].image = starIcon
-            items[1].image = settingsIcon
+            
+            let starImage = starIcon.image(1)
+            let settingsImage = settingsIcon.image()
+            
+            items[0].image = starImage
+            items[1].image = settingsImage
+            
+            items[0].selectedImage = starImage
+            items[1].selectedImage = settingsImage
         }
+        
     }
 
 }
