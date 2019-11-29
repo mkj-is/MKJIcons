cdfaf5af6dfdb0af176bf18b68180ee6d636de97
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-11-29 16:06:07 +0100

    Fix warnings

--- Sources/MKJIcons/Extensions/UIColor+IconColors.swift
+++ Sources/MKJIcons/Extensions/UIColor+IconColors.swift
@@ -4,19 +4,19 @@ public extension UIColor {
 
     // MARK: - Basic icon colors
 
-    public static var iconRedColor: UIColor {
+    static var iconRedColor: UIColor {
         return UIColor(red: 1.000, green: 0.270, blue: 0.270, alpha: 1.000)
     }
 
-    public static var iconOrangeColor: UIColor {
+    static var iconOrangeColor: UIColor {
         return iconRedColor.colorWithHueComponent(hue: iconRedColor.hue + 0.1)
     }
 
-    public static var iconLightGreenColor: UIColor {
+    static var iconLightGreenColor: UIColor {
         return iconRedColor.colorWithHueComponent(hue: iconRedColor.hue + 0.2)
     }
 
-    public static var iconCyanColor: UIColor {
+    static var iconCyanColor: UIColor {
         return iconRedColor.colorWithHueComponent(hue: iconRedColor.hue + 0.4)
     }
 
--- Sources/MKJIcons/Main classes/AnimatedIcon+Appearance.swift
+++ Sources/MKJIcons/Main classes/AnimatedIcon+Appearance.swift
@@ -12,60 +12,59 @@ public extension AnimatedIcon {
 
     // MARK: - Appearance protocol support
 
-    public func appearanceLineWidth() -> CGFloat {
+    func appearanceLineWidth() -> CGFloat {
         return lineWidth
     }
 
-    public func setAppearanceLineWidth(property: CGFloat) {
+    func setAppearanceLineWidth(property: CGFloat) {
         lineWidth = property
     }
 
-    public func appearanceFillAlpha() -> CGFloat {
+    func appearanceFillAlpha() -> CGFloat {
         return fillAlpha
     }
 
-    public func setAppearanceFillAlpha(property: CGFloat) {
+    func setAppearanceFillAlpha(property: CGFloat) {
         fillAlpha = property
     }
 
-    public func appearanceLineCapStyle() -> Int {
+    func appearanceLineCapStyle() -> Int {
         return lineCapStyle
     }
 
-    public func setAppearanceLineCapStyle(property: Int) {
+    func setAppearanceLineCapStyle(property: Int) {
         lineCapStyle = property
     }
 
-    public func appearanceLineJoinStyle() -> Int {
+    func appearanceLineJoinStyle() -> Int {
         return lineJoinStyle
     }
 
-    public func setAppearanceLineJoinStyle(property: Int) {
+    func setAppearanceLineJoinStyle(property: Int) {
         lineJoinStyle = property
     }
 
-    public func appearanceAnimationDuration() -> CGFloat {
+    func appearanceAnimationDuration() -> CGFloat {
         return CGFloat(animationDuration)
     }
 
-    public func setAppearanceAnimationDuration(property: CGFloat) {
+    func setAppearanceAnimationDuration(property: CGFloat) {
         animationDuration = Double(property)
     }
 
-    public func appearanceAnimationTimingFunction() -> Int {
+    func appearanceAnimationTimingFunction() -> Int {
         return animationTimingFunction
     }
 
-    public func setAppearanceAnimationTimingFunction(property: Int) {
+    func setAppearanceAnimationTimingFunction(property: Int) {
         animationTimingFunction = property
     }
 
-    public func appearanceAnimationColorMode() -> Int {
+    func appearanceAnimationColorMode() -> Int {
         return animationColorMode
     }
 
-    public func setAppearanceAnimationColorMode(property: Int) {
+    func setAppearanceAnimationColorMode(property: Int) {
         animationColorMode = property
     }
-
 }
